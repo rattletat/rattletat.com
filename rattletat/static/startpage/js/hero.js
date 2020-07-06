5554398d5c9bb1f7afbea8622e0671caffbad6b0
Michael Brauweiler <michael.brauweiler@posteo.de>
2020-07-06 04:24:42 +0200

    Add two Heros

--- rattletat/static/startpage/js/hero.js
+++ rattletat/static/startpage/js/hero.js
@@ -1,54 +1,130 @@
+function randomHero() {
+    // heros = [makeLissajous, makeLorenz]
+    heros = [makeLissajous, makeLorenz]
+    var hero = heros[Math.floor(Math.random() * heros.length)];
+    hero()
+}
+
 function makeLissajous() {
+
+    // Sizing
     var width = window.innerWidth;
     var height = window.innerHeight * .975;
+
     window.onresize = function () {
         width = window.innerWidth;
         height = window.innerHeight * .975;
     };
-    var svg = d3.select("#hero");
+
+    var svg = d3.select("#hero").style('background', 'black');
+
+    // Color
+    svg.append("linearGradient")
+        .attr("id", "line-gradient")
+        .selectAll("stop")
+        .data([
+            {offset: "0%", color: "#b2182b"},
+            {offset: "100%", color: "#2166ac"}
+        ])
+        .enter().append("stop")
+        .attr("offset", function (d) {return d.offset;})
+        .attr("stop-color", function (d) {return d.color;});
+
+    var line = d3.line()
+        .curve(d3.curveCardinal)
+        .x(d => d[0])
+        .y(d => d[1]);
+
+    var track = svg.append("path")
+        .attr("stroke-width", 8);
+
+    var data = [];
+    function draw(currentX, currentY) {
+        data.push([currentX, currentY]);
+        track.attr("d", line(data)).attr("stroke", "url(#line-gradient)");
+    }
+
+    // Lissajous
     var a = 3.2,
-        b = 5.9; // Lissajous frequencies
-    var phi,
-        omega = (2 * Math.PI) / 6000;
-
-    var currentX = width + width * .05,
-        currentY = height / 2 + height * .05;
-    var previousX = currentX,
-        previousY = currentY;
-
-    var start_line = function (t) {
-        // Calculate position
+        b = 5.9,
+        t = 0,
+        omega = (2 * Math.PI) / 1000;
+    function lissajous() {
+        t += 5;
+        if (2500 <= t && t < 6570) {
+            t = 6570;
+        } else if (7500 <= t && t < 8200) {
+            t = 8440;
+        } else if (t >= 10000) {
+            interval.stop()
+        }
         phi = omega * t;
-        currentX = width / 2 + (width / 2) * Math.cos(a * phi);
-        currentY = height / 2 + (height / 2) * Math.sin(b * phi);
+        draw(
+            currentX = width / 2 + (width / 2) * Math.cos(a * phi),
+            currentY = height / 2 + (height / 2) * Math.sin(b * phi)
+        );
+    }
 
-        // Add next step line
-        line = svg.append("line")
-            .attr("x1", previousX)
-            .attr("y1", previousY)
-            .attr("x2", currentX)
-            .attr("y2", currentY)
-            .attr("stroke-width", 20)
-            .attr("stroke-opacity", 1)
-            .attr("stroke", "white");
+    interval = d3.interval(lissajous, 30);
+}
 
-        line.transition()
-            .duration(300)
-            .attr("stroke", "red")
-            .transition()
-            .duration(12000)
-            .attr("stroke-opacity", 0.03)
-            .attr("stroke-width", 100)
-            .attr("stroke", "blue")
-            .remove();
+function makeLorenz() {
 
-        // Exchange start & end point
-        previousX = currentX;
-        previousY = currentY;
+    // Sizing
+    var width = window.innerWidth;
+    var height = window.innerHeight * .975;
 
+    window.onresize = function () {
+        width = window.innerWidth;
+        height = window.innerHeight * .975;
     };
 
-    d3.interval(start_line, 40);
+    var svg = d3.select("#hero").style('background', 'black');
+
+    // Color
+    svg.append("linearGradient")
+        .attr("id", "line-gradient")
+        .selectAll("stop")
+        .data([
+            {offset: "20%", color: "#b2182b"},
+            {offset: "80%", color: "#2166ac"}
+        ])
+        .enter().append("stop")
+        .attr("offset", function (d) {return d.offset;})
+        .attr("stop-color", function (d) {return d.color;});
+
+
+    var track = svg.append("path")
+        .attr("stroke-width", 8);
+
+    var line = d3.line()
+        .curve(d3.curveCardinal)
+        .x(d => d[0])
+        .y(d => d[1]);
+
+
+    var rho = 2 * 28.0,
+        sigma = 10.0,
+        beta = 8.0 / 3.0,
+        x = 0,
+        y = z = 1,
+        t = 0.01,
+        data = [];
+
+    function draw(x, y, z) {
+        data.push([(width / 2 + 40 * x), (3/2 * height + 15 * -z)]);
+        track.attr("d", line(data)).attr("stroke", "url(#line-gradient)");
+    }
+
+    function lorentz() {
+        draw(
+            x += t * (sigma * (y - x)),
+            y += t * (x * (rho - z) - y),
+            z += t * (x * y - beta * z)
+        );
+    }
+
+    interval = d3.interval(lorentz, 30);
 }
 
 function bounceArrow() {
