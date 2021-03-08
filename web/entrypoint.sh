59076aa3d4ba90e4803ace8668a9bae433e8b388
Michael Brauweiler <michael.brauweiler@posteo.de>
2021-03-08 23:36:35 +0100

    Improve volume handling

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -13,7 +13,7 @@ fi
 
 # python manage.py flush --no-input
 python manage.py migrate
-python manage.py collectstatic --noinput
+python manage.py collectstatic --clear --noinput
 python manage.py compress
 
 exec "$@"
