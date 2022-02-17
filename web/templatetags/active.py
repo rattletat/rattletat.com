235454ebfc70e195bc3ae0a1ca69ee69986cca16
Michael Brauweiler <michael.brauweiler@posteo.de>
2022-02-17 06:54:42 +0000

    Fix templatetag

--- web/templatetags/active.py
+++ web/templatetags/active.py
@@ -5,10 +5,10 @@ register = Library()
 
 
 @register.simple_tag
-def active(request, url):
+def active(request_path, url):
     rev_url = reverse(url)
-    if rev_url != "/" and rev_url in request.path:
+    if rev_url != "/" and rev_url in request_path:
         return "active"
-    elif rev_url == request.path:
+    elif rev_url == request_path:
         return "active"
     return ""
