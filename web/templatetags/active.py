4fd79e964f499c5cdeef31e31bc9adbf160168cf
Michael Brauweiler <michael.brauweiler@posteo.de>
2022-02-17 06:51:49 +0000

    Readd active link

--- web/templatetags/active.py
+++ web/templatetags/active.py
@@ -7,8 +7,8 @@ register = Library()
 @register.simple_tag
 def active(request, url):
     rev_url = reverse(url)
-    # if rev_url != "/" and rev_url in request.path:
-    #     return "active"
-    # elif rev_url == request.path:
-    #     return "active"
+    if rev_url != "/" and rev_url in request.path:
+        return "active"
+    elif rev_url == request.path:
+        return "active"
     return ""
