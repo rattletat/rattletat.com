996db9d539318290398d47cfc46f345369c8e6f6
Michael Brauweiler <michael.brauweiler@posteo.de>
2023-07-25 17:09:21 +0200

    Change maximal distance for dots to disappear

--- web/apps/startpage/static/startpage/js/hero.js
+++ web/apps/startpage/static/startpage/js/hero.js
@@ -143,7 +143,7 @@ function makeDots() {
   const initialBlueDotSpeed = 3;
   const wallRepulsionDistance = 100;
   const escapeThreshold = 100;
-  let maxDistance = Math.sqrt(width * width + height * height);
+  let maxDistance = Math.sqrt(width * width + height * height) * 0.75;
   let colorScale = d3
     .scalePow()
     .domain([0, maxDistance])
